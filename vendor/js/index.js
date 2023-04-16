fbdcbd663c22aa348494d55be9b1eb7085969102
VadimLitau <vadimlitau@yandex.ru>
2023-04-16 21:14:32 +0600

    шапка и мобильное меню

--- vendor/js/index.js
+++ vendor/js/index.js
@@ -1,61 +1,100 @@
-const photo = document.querySelectorAll('.gallery__item');
-const popup = document.querySelector('.popupSlider-wrap');
-const popupCross = document.querySelector('.popupSlider-wrap__cross');
+const photo = document.querySelectorAll(".gallery__item");
+const popup = document.querySelector(".popupSlider-wrap");
+const popupCross = document.querySelector(".popupSlider-wrap__cross");
 
-for(let i = 0; i <= photo.length; i ++){
-  photo[i]?.addEventListener('click', () => {
-    popup.classList.remove('popupSlider-wrap_disabled')
-   count = photo[i].id.slice(6, 7);
-   init();
-  }) 
+// Gallery
+
+for (let i = 0; i <= photo.length; i++) {
+    photo[i]?.addEventListener("click", () => {
+        popup.classList.remove("popupSlider-wrap_disabled");
+        count = photo[i].id.slice(6, 7);
+        init();
+    });
 }
 
-popupCross.addEventListener('click', () => {
-  popup.classList.add('popupSlider-wrap_disabled');
-})
+popupCross.addEventListener("click", () => {
+    popup.classList.add("popupSlider-wrap_disabled");
+});
 
-const images = document.querySelectorAll('.popupSlider-wrap .popupSlider .popupSlider__line img');
-const sliderLine = document.querySelector('.popupSlider__line');
+const images = document.querySelectorAll(
+    ".popupSlider-wrap .popupSlider .popupSlider__line img"
+);
+const sliderLine = document.querySelector(".popupSlider__line");
 let count;
 let width;
 
-function init(){
-  console.log('resize');
-  width = document.querySelector('.popupSlider').offsetWidth;
-  sliderLine.style.width = width * images.length + 'px';
-  images.forEach((item)=>{
-    item.style.width = width + 'px';
-    item.style.height = 'auto';
-  })
+function init() {
+    console.log("resize");
+    width = document.querySelector(".popupSlider").offsetWidth;
+    sliderLine.style.width = width * images.length + "px";
+    images.forEach((item) => {
+        item.style.width = width + "px";
+        item.style.height = "auto";
+    });
 
-  rollSlider();
+    rollSlider();
 }
 
-window.addEventListener('resize', init);
-document.querySelector('.popupSlider__button-right').addEventListener('click', function(){
-  count++;
-  if(count >= images.length) {
-    count =0;
-  }
-  rollSlider()
-})
+window.addEventListener("resize", init);
+document
+    .querySelector(".popupSlider__button-right")
+    .addEventListener("click", function() {
+        count++;
+        if (count >= images.length) {
+            count = 0;
+        }
+        rollSlider();
+    });
 
-document.querySelector('.popupSlider__button-left').addEventListener('click', function(){
-  count--;
-  if(count < 0) {
-    count = images.length - 1;
-  }
-  rollSlider()
-})
+document
+    .querySelector(".popupSlider__button-left")
+    .addEventListener("click", function() {
+        count--;
+        if (count < 0) {
+            count = images.length - 1;
+        }
+        rollSlider();
+    });
 
-function rollSlider(a){
-  sliderLine.style.transform = 'translate(-' + count * width + 'px';
- 
+function rollSlider(a) {
+    sliderLine.style.transform = "translate(-" + count * width + "px";
 }
 
+// mobile nav
 
-//console.log(numIdPhoto)
-function resizeGallery (numIdPhoto){}
+const mobileNavButton = document.querySelector('.header__mobile-nav-button');
+const closeNavButton = document.querySelector('.popup-mobile-nav__closeIcon');
+closeNavButton.addEventListener('click', () => {
+    document.querySelector('.popup-mobile-nav').classList.add('popup-mobile-nav_show');
+})
+mobileNavButton.addEventListener('click', () => {
+    document.querySelector('.popup-mobile-nav').classList.remove('popup-mobile-nav_show');
+})
+// 
 
+const mobileButton = document.getElementById('#mobileMenuServices');
 
+let itemContains =  document.querySelector('.popup-mobile-nav__listDropped').classList.contains('popup-mobile-nav__listDropped_show') 
 
+function openServices () {
+        document.querySelector('.popup-mobile-nav__listDropped').classList.add('popup-mobile-nav__listDropped_show');
+        mobileButton.removeEventListener('click', openServices)        
+        mobileButton.addEventListener('click', closeServices);
+}
+
+function closeServices () {
+        document.querySelector('.popup-mobile-nav__listDropped').classList.remove('popup-mobile-nav__listDropped_show');
+        mobileButton.removeEventListener('click', closeServices)       
+        mobileButton.addEventListener('click', openServices);
+}
+
+if(!itemContains){
+    mobileButton.addEventListener('click', openServices);
+}
+
+const popupMenuItems = document.querySelectorAll('.closePopup');
+popupMenuItems.forEach( item => {
+    item.addEventListener('click', () => {
+        document.querySelector('.popup-mobile-nav').classList.add('popup-mobile-nav_show');
+    })
+})
